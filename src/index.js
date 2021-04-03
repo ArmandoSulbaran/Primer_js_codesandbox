b9bb7b2b49ce539db257974be6afdc29cfb55a69
Armando Sulbaran <ajsulbaran72@gmail.com>
2021-04-03 12:05:08 -0400

    actualizaciones básicas se cambio css tipo de fuente, un h2 en html y texto en js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hola Vanilla!</h1>
 <div>
+  Realizando modificaciones.
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
